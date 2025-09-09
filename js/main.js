8e2badebe72b6110f1ffe24437dfb20e0008acb7
uyanda-maseti <128975995+uyanda-maseti@users.noreply.github.com>
2025-09-09 07:59:49 +0000

    Finish mobile view

--- js/main.js
+++ js/main.js
@@ -11,3 +11,12 @@ function opentab(tabname){
     event.currentTarget.classList.add('active-link');
     document.getElementById(tabname).classList.add('active-tab');
 }
+
+const sideMenu = document.getElementById("side-menu");
+
+function openMenu(){
+    sideMenu.style.right = "0";
+}
+function closeMenu(){
+    sideMenu.style.right = "-200px";
+}
