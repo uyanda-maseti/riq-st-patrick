84087c68f4f813f4e1591491ed0cb69a87f155e6
uyanda-maseti <128975995+uyanda-maseti@users.noreply.github.com>
2025-09-09 09:05:52 +0000

    Align logo on mobile view

--- js/main.js
+++ js/main.js
@@ -20,3 +20,21 @@ function openMenu(){
 function closeMenu(){
     sideMenu.style.right = "-200px";
 }
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbyVGbEdOgPWs0CCOKR60uRo3P4LYnSEnlOX401_fw9Q45pZ1NrgnrrKVT3LjYGb3xetFg/exec'
+  const form = document.forms['submit-to-google-sheet'];
+
+  const msg = document.getElementById("msg");
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully";
+        setTimeout(function(){
+            msg.innerHTML = "";
+        },5000);
+        form.reset();
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
